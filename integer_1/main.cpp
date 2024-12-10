bbaed89da8a105a3b2f9925ac0a12bb66eb4d880
obrechen <kostyacheek1708@gmail.com>
2024-12-10 21:50:45 +0300

    integer

---
+++ integer_1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+    int L;
+    std::cout << "Введите L: ";
+    std::cin >> L;
+    int A = L/100;
+    std::cout << "Значение L в метрах равно = " << A;
+    return 0;
+}
+
