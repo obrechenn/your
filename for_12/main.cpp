bf8060f3624b3337dd7328f0685f7fd086a0721a
obrechen <kostyacheek1708@gmail.com>
2025-01-26 22:31:33 +0300

    update

--- for_12/main.cpp
+++ for_12/main.cpp
@@ -5,10 +5,9 @@ int main()
     int n;
     std::cout<<"введите значение числа n: ";
     std::cin>>n;
-    double summ = 0;
-    for(double i = 1; i < n;i++){
-        i -= 0.9;
-        summ += i * (i + 0.1);
+    double summ = 1;
+    for(double i = 1.1; i < n;i += 0.1){
+        summ *= i;
     }
 std::cout<<summ<<'\n';
     return 0;
